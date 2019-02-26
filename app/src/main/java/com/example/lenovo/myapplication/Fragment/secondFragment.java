449b6d277fa8378a73d188a1094a857e60ff32a5
cuihengyuan <1210002860@qq.com>
2019-02-26 15:27:44 +0800

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/lenovo/myapplication/Fragment/secondFragment.java
+++ app/src/main/java/com/example/lenovo/myapplication/Fragment/secondFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +12,26 @@ import android.view.ViewGroup;
 import com.example.lenovo.myapplication.R;
 
 public class secondFragment extends Fragment {
+//    SearchView searchView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.secondfragment_layout, container, false);
+//    searchView=view.findViewById(R.id.searchView);
+//   // searchView.setImeOptions(0);
+//searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//    @Override
+//    public boolean onQueryTextSubmit(String s) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean onQueryTextChange(String s) {
+//        return false;
+//    }
+//});
+
+
         return view;
     }
 }
